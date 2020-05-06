281a184b66d1cd0e647132c8060ae8b29c88dbe2
Nicolo Maioli <nmaioli@and.digital>
2020-05-06 01:29:08 +0100

    Add paste command

---
+++ cmd/paste.go
@@ -0,0 +1,64 @@
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var pasteReg string
+
+func init() {
+	rootCmd.AddCommand(pasteCmd)
+
+	// Address
+	pasteCmd.Flags().StringP("address", "a", ":8080", "address of the clipd server")
+	viper.BindPFlag("client.address", pasteCmd.Flags().Lookup("address"))
+	pasteCmd.Flags().StringVarP(&pasteReg, "register", "r", "", "named register")
+}
+
+var pasteCmd = &cobra.Command{
+	Use:   "paste",
+	Short: "Paste content from a register",
+	Long: `
+Paste content from a register. If a named register is not provided, the default
+register ("default") is used.
+	`,
+	Args: cobra.NoArgs,
+	Run: func(cmd *cobra.Command, args []string) {
+		clipdAddr := viper.GetString("client.address")
+		url := fmt.Sprintf("http://%s/clipd", clipdAddr)
+
+		if pasteReg != "" {
+			url += fmt.Sprintf("/%s", pasteReg)
+		}
+
+		res, err := http.Get(
+			url,
+		)
+
+		if res != nil {
+			defer res.Body.Close()
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if res.StatusCode != http.StatusOK {
+			log.Fatalf("http error %d", res.StatusCode)
+		}
+
+		content, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Fprint(os.Stdout, string(content))
+	},
+}
